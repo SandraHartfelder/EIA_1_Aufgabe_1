222edd018157e88bd0e55647647237cb62e28368
SandraHartfelder <shartfelder@web.de>
2023-12-02 23:12:38 +0100

    L05

---
+++ code.js
@@ -0,0 +1,12 @@
+let decision;
+
+prompt('du bist in einem wald und kommst an eine kreuzung. Wohin gehst du?');
+
+if (decision == 'left') {
+    prompt('du trisst auf einen blauäugigen drachen. willst du kämpfen oder fliehen?');
+ } else if (decision == 'right') {
+    prompt('du triffst einen alten Mann und er bietet dir an dich zu trainieren. nimmst du an oder lehnst du ab?');
+ }else {
+    prompt('Keine korrekte Eingabe');
+ }
+
