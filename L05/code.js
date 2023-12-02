da215c194a97c970bc0f36b1b46f8fc061284e59
SandraHartfelder <shartfelder@web.de>
2023-12-03 00:51:49 +0100

    L05

--- L05/code.js
+++ L05/code.js
@@ -1,11 +1,14 @@
-let decision;
-
-prompt('du bist in einem wald und kommst an eine kreuzung. Wohin gehst du?');
+let decision = prompt('Du spazierst durch einen Wald und kommst an eine Weggabelung. Wohin gehst du?');
+// Zeigt eine Variable an und prompt wird der Variable zugewiesen.
 
 if (decision == 'left') {
-    prompt('du trisst auf einen blauäugigen drachen. willst du kämpfen oder fliehen?');
- } else if (decision == 'right') {
-    prompt('du triffst einen alten Mann und er bietet dir an dich zu trainieren. nimmst du an oder lehnst du ab?');
- }else {
-    prompt('Keine korrekte Eingabe');
- }
+    alert('Du hast dich dafür entschieden nach links zu gehen und triffst auf einen blauäugigen weißen Drachen, welchen du zähmst.');
+}
+ else (decision == 'right'); { alert('Du hast dich entschieden nach rechts zu gehen und findest eine myteriöse Frucht, welche dir geheimnisvolle Kräfte verspricht.');
+} 
+
+// Bei dem if wird geprüft, ob die Person nach links geht. Wenn nicht wird bei else geprüft, ob sie nach rechts geht. 
+// Je nach Entscheidung wird durch alert ein anderer Verlauf der Geschichte ausgegeben. 
+
+ alert('Nun bist du die stärkste Person im ganzen Land.'); 
+// Hier wird das Ende der Geschichte angezeigt.
