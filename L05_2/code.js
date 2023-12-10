431b8df67adf200a6c18d0bd2bc724e88b7bad66
SandraHartfelder <shartfelder@web.de>
2023-12-10 11:16:30 +0100

    L05_2

---
+++ L05_2/code.js
@@ -0,0 +1,21 @@
+let myArray = ['Du spazierst durch einen Wald und kommst an eine Weggabelung. Wohin gehst du?','Du hast dich dafür entschieden nach links zu gehen und triffst auf einen blauäugigen weißen Drachen, welchen du zähmst.',
+'Du hast dich entschieden nach rechts zu gehen und findest eine myteriöse Frucht, welche dir geheimnisvolle Kräfte verspricht.', 'Nun bist du die stärkste Person im ganzen Land.']
+
+function end(){
+    alert[3];
+}
+
+let decision = prompt[0];
+// Zeigt eine Variable an und prompt wird der Variable zugewiesen.
+
+if (decision == 'left') {
+    alert[1];
+}
+ else (decision == 'right'); { alert[2];
+} 
+
+// Bei dem if wird geprüft, ob die Person nach links geht. Wenn nicht wird bei else geprüft, ob sie nach rechts geht. 
+// Je nach Entscheidung wird durch alert ein anderer Verlauf der Geschichte ausgegeben. 
+
+ end(); 
+// Hier wird das Ende der Geschichte angezeigt.
