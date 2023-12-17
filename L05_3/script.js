297d3cdf7cee7e0c3fc3e395a714f0cf74427398
SandraHartfelder <shartfelder@web.de>
2023-12-17 21:44:01 +0100

    L05_3

--- L05_3/script.js
+++ L05_3/script.js
@@ -1,15 +1,21 @@
-stories = ['Du spazierst durch einen Wald und kommst an eine Weggabelung. Wohin gehst du?','Du hast dich dafür entschieden nach links zu gehen und triffst auf einen blauäugigen weißen Drachen, welchen du zähmst.',
+stories = ['Du hast dich dafür entschieden nach links zu gehen und triffst auf einen blauäugigen weißen Drachen, welchen du zähmst.',
 'Du hast dich entschieden nach rechts zu gehen und findest eine myteriöse Frucht, welche dir geheimnisvolle Kräfte verspricht.', 'Nun bist du die stärkste Person im ganzen Land.']
 
 const input = document.querySelector(".inputForm__field")
 const output = document.getElementById("output");
 
-input.addEventListener("keeydowm", function(event) {
+input.value = ""
+input.addEventListener("keydown", function(event) {
     if (event.key == "Enter") {
         if (input.value == "links") {
             output.innerHTML = stories[0]
+            input.value = ""
         } else if (input.value == "rechts") {
             output.innerHTML = stories[1]
+            input.value = ""
+        }else if (input.value == "cool") {
+            output.innerHTML = stories[2]
+            input.value = ""
         }
     }
 })
